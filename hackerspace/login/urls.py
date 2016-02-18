a2fd0805e86c67548a331e6184e5f33f6b25cb32
Jeremy Jackson <jj.cseng@gmail.com>
2016-02-18 09:37:24 +0530

    creates registration action

--- hackerspace/login/urls.py
+++ hackerspace/login/urls.py
@@ -7,4 +7,5 @@ app_name = 'login'
 urlpatterns = [
     url(r'^$', views.login, name='login'),
     url(r'^validate', views.validate, name='validate'),
+    url(r'^register', views.register, name='register'),
 ]
--- hackerspace/login/views.py
+++ hackerspace/login/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate
 from django.contrib.auth.models import User
+from django.http import HttpResponse
+import re
 
 # Create your views here.
 def login(request):
@@ -11,3 +13,11 @@ def validate(request):
     if user is not None:
         if user.is_active:
             return render(request,'/feed/')
+    else:
+        return HttpResponse("You are not registered.")
+
+def register(request):
+    registration_details = request.POST
+    if( registration_details['password'] == registration_details['rpassword'] ):
+        user = User.objects.create_user(registration_details['username'],registration_details['password'])
+        user.save();
